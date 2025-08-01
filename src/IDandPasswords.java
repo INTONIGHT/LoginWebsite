7137928fcc306e660cc6e4ee9f76c01a37c2171f
INTONIGHT <tyler.throop@gmail.com>
2025-08-01 09:24:43 -0700

    getting things started

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
 
 public class IDandPasswords {
-
+	HashMap<String,String> logininfo = new HashMap<String,String>();
+	
+	IDandPasswords(){
+		
+		logininfo.put("Test","pass");
+		logininfo.put("Brother","password");
+	}
+	protected HashMap getLoginInfo(){
+		return logininfo;
+	}
 }
--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,4 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class LoginPage {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
+public class LoginPage implements ActionListener{
+	JFrame frame = new JFrame();
+	JButton loginButton = new JButton("Login");
+	JButton resetButton = new JButton("Reset");
+	JTextField userIdField = new JTextField();
+	JPasswordField userPasswordField = new JPasswordField();
+	JLabel userIdLabel = new JLabel("UserId:");
+	JLabel userPasswordLabel = new JLabel("Password:");
+	JLabel message = new JLabel();
+	
+	HashMap<String,String> loginInfoCopy = new HashMap<String,String>();
+	
+	LoginPage(HashMap<String,String> loginInfo){
+		loginInfoCopy = loginInfo;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }

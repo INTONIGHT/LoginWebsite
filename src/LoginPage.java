636f03bf41616fcb4819e43f7794734aa864a4a3
INTONIGHT <tyler.throop@gmail.com>
2025-08-01 09:39:15 -0700

    This of course is a very simple example but its good practice

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -16,18 +18,61 @@ public class LoginPage implements ActionListener{
 	JPasswordField userPasswordField = new JPasswordField();
 	JLabel userIdLabel = new JLabel("UserId:");
 	JLabel userPasswordLabel = new JLabel("Password:");
-	JLabel message = new JLabel();
+	JLabel messageLabel = new JLabel();
 	
 	HashMap<String,String> loginInfoCopy = new HashMap<String,String>();
 	
 	LoginPage(HashMap<String,String> loginInfo){
 		loginInfoCopy = loginInfo;
 		
+		userIdLabel.setBounds(50,100,75,25);
+		userPasswordLabel.setBounds(50,150,75,25);
+		messageLabel.setBounds(125,250,250,35);
+		messageLabel.setFont(new Font(null,Font.ITALIC,25));
+		
+		userIdField.setBounds(125,100,200,25);
+		userPasswordField.setBounds(125,150,200,25);
+		
+		loginButton.setBounds(125, 200, 100, 25);
+		loginButton.setFocusable(false);
+		loginButton.addActionListener(this);
+		
+		resetButton.setBounds(225, 200, 100, 25);
+		resetButton.setFocusable(false);
+		resetButton.addActionListener(this);
+		
+		frame.add(userIdLabel);
+		frame.add(userPasswordLabel);
+		frame.add(messageLabel);
+		frame.add(userIdField);
+		frame.add(userPasswordField);
+		frame.add(loginButton);
+		frame.add(resetButton);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if(e.getSource() == resetButton) {
+			userIdField.setText("");
+			userPasswordField.setText("");
+		}
+		if(e.getSource() == loginButton) {
+			String userID = userIdField.getText();
+			String userPassword = String.valueOf(userPasswordField.getPassword());
+			if(loginInfoCopy.containsKey(userID)) {
+				if(loginInfoCopy.get(userID).equals(userPassword)) {
+					messageLabel.setForeground(Color.green);
+					messageLabel.setText("Login Successful");
+					WelcomePage welcomePage = new WelcomePage();
+				}
+			}
+		}
 		
 	}
 }
